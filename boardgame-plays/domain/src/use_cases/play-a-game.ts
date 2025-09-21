2df29b3d8789588081a392754c6616109d608ddb
nanou <anael.chardan@gmail.com>
2025-09-21 15:36:24 +0200

    1_functionnal: implementation du port primaire + tests

--- boardgame-plays/domain/src/use_cases/play-a-game.ts
+++ boardgame-plays/domain/src/use_cases/play-a-game.ts
@@ -1,6 +1,44 @@
 import { Play } from "../model/play";
+import { BoardgameInventory } from "../secondary_ports/boardgame-inventory";
 
 // This is the left side port of the hexagon
 export type PlayAGame = {
   forBoardgame: (boardgameName: string, players: string[]) => Promise<Play>;
 };
+
+type Dependencies = {
+  boardgameInventory: BoardgameInventory;
+};
+
+export function buildPlayAGame({
+  boardgameInventory,
+}: Dependencies): PlayAGame {
+  async function forBoardgame(
+    boardgameName: string,
+    players: string[]
+  ): ReturnType<PlayAGame["forBoardgame"]> {
+    const boardgame =
+      await boardgameInventory.getBoardgameByName(boardgameName);
+
+    if (!boardgame) {
+      throw new Error("Boardgame not found");
+    }
+
+    if (
+      players.length < boardgame.minNumberOfPlayers ||
+      players.length > boardgame.maxNumberOfPlayers
+    ) {
+      throw new Error("Invalid number of players");
+    }
+
+    return {
+      boardgameName,
+      bggId: boardgame.bggId,
+      players,
+    };
+  }
+
+  return {
+    forBoardgame,
+  };
+}
