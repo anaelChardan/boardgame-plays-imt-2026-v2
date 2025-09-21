000d8653aa5f1ec0d980ee349ff408a57ebcee64
nanou <anael.chardan@gmail.com>
2025-09-21 17:41:42 +0200

    2_controller: extract stub

--- boardgame-plays/domain/src/__tests__/functionnal/play-a-game.test.ts
+++ boardgame-plays/domain/src/__tests__/functionnal/play-a-game.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { Play } from "../../model/play";
 import { PlayAGame, buildPlayAGame } from "../../use_cases/play-a-game";
+import { boardgameInventoryStub } from "../../secondary_ports/boardgame-inventory";
 
 describe("Play a game functionnal tests", () => {
   it("should play a game with the correct number of players", async () => {
@@ -11,16 +12,7 @@ describe("Play a game functionnal tests", () => {
     // When
     // le but est d'appeler le domain
     const playAGame: PlayAGame = buildPlayAGame({
-      boardgameInventory: { // injection d'un stup
-        getBoardgameByName: async (name: string) => {
-          return {
-            name,
-            bggId: "224517",
-            maxNumberOfPlayers: 4,
-            minNumberOfPlayers: 2,
-          };
-        },
-      },
+      boardgameInventory: boardgameInventoryStub,
     });
 
     const play: Play = await playAGame.forBoardgame(boardgameName, players);
